b8712462ccd99d564b233230e150e538c9cdcd59
Enrico Pittini <pittinienrico@hotmail.it>
2022-09-17 22:36:51 +0200

    Add Roi refinement (step 5)

--- 2) ROI refinement/5) find_wrong_bar.py
+++ 2) ROI refinement/5) find_wrong_bar.py
@@ -6,7 +6,7 @@ import math
 import matplotlib.patches as patches
 import time
 
-def find_outliers(v, level=0.02):
+def _find_outliers(v, level=0.02):
     q1, q3 = tuple(np.quantile(v, [level,1-level]))
     IQR = q3-q1
     outliers_mask = np.logical_or(v>q3+IQR, v<q1-IQR)
@@ -21,10 +21,10 @@ def find_wrong_bar(bars_start, bars_width, bars_halfHeightUp, bars_halfHeightDow
     n_bars = len(bars_start)
     bars_height = np.array([bars_halfHeightUp[i]+bars_halfHeightDown[i]+1 for i in range(n_bars)])
     #print(bars_height)
-    wrong_bars_height_index = find_outliers(bars_height, level=0.02)
+    wrong_bars_height_index = _find_outliers(bars_height, level=0.02)
     
     bars_area = np.array([bars_height[i]+bars_width[i] for i in range(n_bars)])
-    wrong_bars_area_index = find_outliers(bars_area, level=0.02)
+    wrong_bars_area_index = _find_outliers(bars_area, level=0.02)
     
     if wrong_bars_area_index is None or wrong_bars_height_index is None or wrong_bars_area_index!=wrong_bars_height_index:
         return None
