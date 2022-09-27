a59b9e50e6633c3fcb081e531cf6343bb2d5bf6f
Enrico Pittini <pittinienrico@hotmail.it>
2022-09-27 23:34:45 +0200

    Building output file

---
+++ 6) Actual final folder/build_output_file.py
@@ -0,0 +1,86 @@
+
+import os
+import json
+import pandas as pd
+import numpy as np
+
+
+def _compute_boundingBox_centre(bb_points_sorted):
+    return np.mean(bb_points_sorted, axis=0)
+
+def _compute_barsSpaces_widths(barcode_structure_dict):
+    X = barcode_structure_dict['X']
+
+    barsSpaces_widths = []
+    current_space_start = None
+    for bar_start, bar_width in zip(barcode_structure_dict['bars_start'], barcode_structure_dict['bars_width']):
+        barsSpaces_widths.append(bar_width)
+        if current_space_start is not None:
+            current_space_width = bar_start-current_space_start
+            barsSpaces_widths.append(current_space_width)
+        current_space_start = bar_start + bar_width
+
+    spacesBars_flags = [True if i%2==0 else False for i in range(len(barsSpaces_widths))]
+
+    return {
+        'bars_spaces_widths': barsSpaces_widths,
+        'bars_spaces_flags': spacesBars_flags
+    }
+
+
+
+def build_output_file(detection_dict, rotation_dict, refinement_dict, overall_quality_parameters_dict, image_name, n_scanlines=10,
+        output_file_name=None, output_file_type='excel', output_folder_path='./out'):
+
+    #output_folder_path = os.path.normpath(output_folder_path)
+
+    if output_file_name is None:
+        output_file_name = 'output ' + image_name
+    output_file_name += '.xlsx' if output_file_type=='excel' else '.json'
+    output_path = os.path.join(output_folder_path, output_file_name)
+
+    print(output_path)
+    
+    if output_file_type=='excel':
+        global_quantities_df = pd.DataFrame({
+            'image_name' : image_name,
+            'bb_points_sorted': [detection_dict['bb_points_sorted']],
+            'bb_centre' : [_compute_boundingBox_centre(detection_dict['bb_points_sorted'])],
+            'angle' : [rotation_dict['angle']],
+            'X' : [refinement_dict['barcode_structure_dict']['X']],
+            'height' : [refinement_dict['barcode_structure_dict']['height']],
+            'OVERALL_SYMBOL_GRADE' : [overall_quality_parameters_dict['OVERALL_SYMBOL_GRADE']]
+        })
+        
+        """barcode_structure_df = pd.DataFrame({
+            'bars_start': refinement_dict['barcode_structure_dict']['bars_start'],
+            'bars_width': refinement_dict['barcode_structure_dict']['bars_width'],
+            'bars_halfHeightUp': refinement_dict['barcode_structure_dict']['bars_halfHeightUp'],
+            'bars_halfHeightDown': refinement_dict['barcode_structure_dict']['bars_halfHeightDown']
+        })"""
+        barcode_structure_df = pd.DataFrame(_compute_barsSpaces_widths(refinement_dict['barcode_structure_dict']))
+        barcode_structure_df.index.name = 'bars_spaces'
+
+        scanlines_qualityParameters_df = pd.DataFrame([overall_quality_parameters_dict[f'scanline_{i}'] 
+                                                      for i in range(n_scanlines)])
+        scanlines_qualityParameters_df.index.name = 'scanlines'
+
+        with pd.ExcelWriter(output_path) as writer:
+            global_quantities_df.to_excel(writer, sheet_name='Global quantities')
+            barcode_structure_df.to_excel(writer, sheet_name='Bars_spaces widths')
+            scanlines_qualityParameters_df.to_excel(writer, sheet_name='Scanline quality parameters')
+
+    elif output_file_type=='json':
+        output_dict = {
+            'image_name': image_name,
+            'detection_dict': detection_dict,
+            'rotation_dict': rotation_dict,
+            'refinement_dict': refinement_dict,
+            'overall_quality_parameters_dict': overall_quality_parameters_dict
+        } 
+        print(overall_quality_parameters_dict)
+        with open(output_path, 'w') as out_file:
+            json.dump(output_dict, out_file, indent=4)
+
+    else:
+        raise ValueError(f'Unsopported output file type {output_file_type}')
--- 6) Actual final folder/verify_barcode.py
+++ 6) Actual final folder/verify_barcode.py
@@ -1,18 +1,25 @@
 import time
+import os
+import cv2
 
 from detect_boundingBox import detect_boundingBox
 from rotate_boundingBox import rotate_boundingBox
 from refine_ROIimage import refine_ROIimage
 from compute_quality_parameters import compute_quality_parameters
+from build_output_file import build_output_file
 
 """ verbose_timing : bool, optional
         Whether to print the timing information about the process or not, by default False"""
 
 
 
-def verify_barcode(image, use_same_threshold=False, compute_barcode_structure_algorithm=1, n_scanlines=10, 
-                   outlier_detection_level=0.02, visualization_dict=None, verbose_timing=False):
-    
+def verify_barcode(image_path, use_same_threshold=False, compute_barcode_structure_algorithm=1, n_scanlines=10, 
+                   outlier_detection_level=0.02, visualization_dict=None, verbose_timing=False, create_output_file=False,
+                   output_file_name=None, output_file_type='excel', output_folder_path='./out'):
+
+    image = cv2.imread(image_path) 
+    image_name = '.'.join(os.path.basename(image_path).split('.')[:-1])
+    print(image_name)   
 
     visualization_dict = _populate_visualization_dict(visualization_dict) 
 
@@ -51,7 +58,31 @@ def verify_barcode(image, use_same_threshold=False, compute_barcode_structure_al
         print('\tCompute quality parameters:', end_computeQualityParameters_time-end_refineROI_time)
         print()
 
-    return overall_quality_parameters_dict
+    detection_dict = {
+        'bb_points_sorted': bb_points_sorted,
+        'bb_width': bb_width,
+        'bb_height': bb_height,
+        'threshold': threshold
+    }
+
+    rotation_dict = {
+        'image_rot': image_rot,
+        'bb_points_sorted_rot': bb_points_sorted_rot,
+        'roi_image': roi_image,
+        'angle': angle
+    }
+
+    refinement_dict = {
+        'roi_image_ref': roi_image_ref,
+        'bb_points_sorted_rot_ref': bb_points_sorted_rot_ref,
+        'barcode_structure_dict': barcode_structure_dict
+    }
+
+    if create_output_file:
+        build_output_file(detection_dict, rotation_dict, refinement_dict, overall_quality_parameters_dict, image_name, n_scanlines=n_scanlines,
+                           output_file_name=output_file_name, output_file_type=output_file_type, output_folder_path=output_folder_path)
+
+    return detection_dict, rotation_dict, refinement_dict, overall_quality_parameters_dict
 
 
 
